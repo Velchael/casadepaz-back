d0b65d8a0be12a23d3a70eca77430ccbfa2a86fc
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-12-12 23:55:37 -0300

    modificar archivo router

--- backusuario/src/router.js
+++ backusuario/src/router.js
@@ -4,3 +4,4 @@ const usersController = require('./controllers/usersController');
 router.post('/users', usersController.createUsers);
 router.post('/users/login', usersController.getUserByUsernameAndPassword);
 module.exports = router;
+//////
